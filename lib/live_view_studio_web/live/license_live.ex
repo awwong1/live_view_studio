31c0859a6567498317e804fa47e317dc6b488027
Alexander Wong <alex@udia.ca>
2021-04-24 20:26:17 -0600

    Video 6: Dashboard, added a countdown timer exercise

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -1,11 +1,24 @@
 defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
+  use Timex
 
   alias LiveViewStudio.Licenses
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :tick)
+    end
+    # expiration_time = Timex.shift(Timex.now(), hours: 1)
+    expiration_time = Timex.shift(Timex.now(), seconds: 15)
+    socket =
+      assign(
+        socket,
+        seats: 2,
+        amount: Licenses.calculate(2),
+        expiration_time: expiration_time,
+        time_remaining: time_remaining(expiration_time)
+      )
     {:ok, socket}
   end
 
@@ -26,6 +39,11 @@ defmodule LiveViewStudioWeb.LicenseLive do
             <input type="range" min="1" max="10"
                   name="seats" value="<%= @seats %>" />
           </form>
+          <%= if @time_remaining > 0 do %>
+          <h3><%= format_time(@time_remaining) %></h3>
+          <% else %>
+          <h3>Sale is over! Thank you.</h3>
+          <% end %>
           <div class="amount">
             <%= number_to_currency(@amount) %>
           </div>
@@ -39,5 +57,21 @@ defmodule LiveViewStudioWeb.LicenseLive do
     seats = String.to_integer(seats)
     socket = assign(socket, seats: seats, amount: Licenses.calculate(seats))
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
